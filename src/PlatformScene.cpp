22a9831160c8d52d5cef8f9dda69cc28b3501afb
Federico De Felici <fdf8210@gmail.com>
2024-06-13 17:13:27 +0200

    refactor

--- src/PlatformScene.cpp
+++ src/PlatformScene.cpp
@@ -1,15 +1,16 @@
 #include "PlatformScene.h"
 #include "OGLQuadRenderer.h"
+#include "OrthoCamera.h"
 
 void PlatformScene::Start() 
 {
     Camera = new OrthoCamera(Win.GetWidth(), Win.GetHeight());
 
     Quad* Q1 = new Quad(100, 10);
-    Q1->Position = glm::vec3{-100, 0, 0};
+    Q1->Position = glm::vec3{300, 300, 0};
 
     Quad* Q2 = new Quad(100, 10);
-    Q2->Position = glm::vec3{100, 0, 0};
+    Q2->Position = glm::vec3{500, 300, 0};
     
     Quads.push_back(Q1);    
     Quads.push_back(Q2);    
